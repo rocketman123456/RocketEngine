2478102d28f34502ed11b213770bd12624d68748
Rocketman <759094438@qq.com>
2021-04-26 02:50:47 +0800

    update xmake

---
+++ RocketEngine/xmake.lua
@@ -0,0 +1,12 @@
+target("RocketEngine")
+    --set_kind("static")
+    set_kind("binary")
+    add_files("*.cpp")
+    if is_plat("linux") then
+        add_links("pthread", "m", "dl")
+    end
+    if is_plat("macosx") then
+        add_links("pthread", "m", "dl")
+    end
+    if is_plat("windows") then
+    end
--- xmake.lua
+++ xmake.lua
@@ -1,75 +1,57 @@
+-- Rocket Engine Config File
+
 add_rules("mode.debug", "mode.release")
+set_languages("c99", "c++17")
+--set_warnings("all", "error")
+--add_includedirs("/usr/include", "/usr/local/include")
+--add_linkdirs("/usr/local/lib", "/usr/lib")
+--add_links("tbox")
+--add_syslinks("z", "pthread")
+--add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
 
-target("RocketEngine")
-    set_kind("binary")
-    add_files("RocketEngine/*.cpp")
+-- 如果当前编译模式是debug
+if is_mode("debug") then
+    -- 添加DEBUG编译宏
+    add_defines("RK_DEBUG")
+    -- 启用调试符号
+    set_symbols("debug")
+    -- 禁用优化
+    set_optimize("none")
+end
+
+-- 如果是release或者profile模式
+if is_mode("release", "profile") then
+    -- 如果是release模式
+    if is_mode("release") then
+        -- 添加DEBUG编译宏
+        add_defines("RK_RELEASE")
+        -- 隐藏符号
+        set_symbols("hidden")
+        -- strip所有符号
+        set_strip("all")
+        -- 忽略帧指针
+        add_cxflags("-fomit-frame-pointer")
+        add_mxflags("-fomit-frame-pointer")
+        -- 优化
+        set_optimize("fastest")
+    -- 如果是profile模式
+    else
+        -- 添加DEBUG编译宏
+        add_defines("RK_PROFILE")
+        -- 启用调试符号
+        set_symbols("debug")
+        -- 优化
+        set_optimize("fastest")
+    end
+    -- 添加扩展指令集
+    add_vectorexts("sse2", "sse3", "ssse3", "mmx")
+end
+
+includes("RocketEngine")
 
 --
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
+-- How to configure project?
 --
 --   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
 --
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro defination
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
