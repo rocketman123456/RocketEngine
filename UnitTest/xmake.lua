3162a2414c2262a152f74953c6c6700baff58b10
Rocketman <759094438@qq.com>
2021-04-27 19:03:29 +0800

    update

--- UnitTest/xmake.lua
+++ UnitTest/xmake.lua
@@ -1,7 +1,7 @@
 target("memory_leak_test")
     set_kind("binary")
     add_files("memory_leak_test.cpp")
-    add_deps("RocketEngine")
+    add_deps("RocketEngine", {inherit = true})
 
 target("glfw_test")
     set_kind("binary")
