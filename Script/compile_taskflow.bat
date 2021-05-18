a8ff4c18371e4b59b927dc73c4e7ab72a21c05c8
rocketman123456 <759094438@qq.com>
2021-05-18 19:39:45 +0800

    remove build script

--- Script/compile_taskflow.bat
+++
@@ -1,6 +0,0 @@
-mkdir Thirdparty\build\taskflow
-pushd Thirdparty\build\taskflow
-del /S /Q *
-cmake -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DCMAKE_INSTALL_PREFIX=..\..\..\External\Windows\ -DCMAKE_INSTALL_RPATH=..\..\..\External\Windows\ -DTF_BUILD_EXAMPLES=OFF -DTF_BUILD_TESTS=OFF ..\..\taskflow
-cmake --build . --config Debug --target install
-popd
--- Script/compile_taskflow.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p Thirdparty/build/taskflow
-pushd Thirdparty/build/taskflow
-rm -rf *
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../../External/`uname -s`/ -DCMAKE_INSTALL_RPATH=../../../External/`uname -s`/ -DTF_BUILD_EXAMPLES=OFF -DTF_BUILD_TESTS=OFF ../../taskflow
-cmake --build . --config Debug --target install
-popd
