eacbe24eb2b7986deb8c4b8c3470478a4cdcd724
Rocketman <759094438@qq.com>
2021-07-13 12:08:47 +0800

    add memory leak check

--- Rocket/Engine/main.cpp
+++ Rocket/Engine/main.cpp
@@ -1,7 +1,36 @@
+#define ENABLE_NEW_DELETE_TRACE_DUMP
+#include "Memory/MemoryCheck.h"
+
 #include <iostream>
+
+template<typename T, int N> void good() {
+	delete (new T);
+	delete [] (new T [N]);
+
+	struct Boom { Boom() { throw 1; } };
+
+	try { new Boom; } catch(...) { } // not a leak!
+	try { new Boom [N]; } catch(...) { } // same!
+}
+
+template<typename T, int N> void mismatch() {
+	delete [] (new T);
+	delete (new T [N]);
+}
+
+template<typename T, int N> void leak() {
+	/*delete*/ new T;
+	/*delete []*/ new T [N];
+}
 
 int main() {
     std::cout << "Hello World" << std::endl;
+
+    good<char, 100>();
+	mismatch<char, 100>();
+	leak<char, 100>();
+
+    new int32_t[1000];
 
     return 0;
 }
