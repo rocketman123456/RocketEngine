d588c21183047165fd8bb8bc2931b4b96e33a528
rocketman123456 <759094438@qq.com>
2021-07-14 23:00:39 +0800

    add binary search

--- Rocket/Engine/Algorism/Sort.h
+++ Rocket/Engine/Algorism/Sort.h
@@ -2,13 +2,15 @@
 #include <functional>
 
 namespace Rocket {
+    // In-place Sort
     template<typename T>
-    void SortBigFirst(T* data, std::size_t size, std::function<bool(T&, T&)>) {
+    void SortBigFirst(T* data, std::size_t size, std::function<bool(T&, T&)> comapre) {
 
     }
 
+    // In-place Sort
     template<typename T>
-    void SortLittleFirst(T* data, std::size_t size, std::function<bool(T&, T&)>) {
+    void SortLittleFirst(T* data, std::size_t size, std::function<bool(T&, T&)> comapre) {
 
     }
 }
--- UnitTest/xmake.lua
+++ UnitTest/xmake.lua
@@ -2,6 +2,10 @@ target("memory_test")
     set_kind("binary")
     add_files("memory_test.cpp")
 
+target("search_test")
+    set_kind("binary")
+    add_files("search_test.cpp")
+
 target("sort_test")
     set_kind("binary")
     add_files("sort_test.cpp")
