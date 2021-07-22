b1065a5d1e1e804ca6dbf2361f10c4fa48cec9c6
rocketman123456 <759094438@qq.com>
2021-07-22 21:10:10 +0800

    begin factory

--- Rocket/Engine/Pattern/Factory.h
+++ Rocket/Engine/Pattern/Factory.h
@@ -2,7 +2,18 @@
 #include "Core/Declare.h"
 
 namespace Rocket {
-    Interface Factory {
+    template<typename T>
+    Interface Product {
+    public:
+        virtual ~Product() = default;
+    };
 
+    template<typename T>
+    Interface Factory {
+    public:
+        virtual ~Factory() = default;
+        virtual T Create() = 0;
+        virtual T CreateProduct() = 0;
+        virtual void RegisterProduct() = 0;
     };
 }
