98ad04badce511ad427cdb7e30cb0904d715c5b0
Rocketman <759094438@qq.com>
2021-08-03 23:51:32 +0800

    fix compile error with hash function

--- UnitTest/ComponentTest/hash_test.cpp
+++ UnitTest/ComponentTest/hash_test.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main() {
     uint64_t h1 = "test hash"_hash;
-    uint64_t h2 = Rocket::hash(std::string("test hash"));
+    uint64_t h2 = Rocket::hash("test hash");
     uint64_t h3 = "test hash"_u64;
-    uint64_t h4 = Rocket::u64(std::string("test hash"));
+    uint64_t h4 = Rocket::u64("test hash");
 
     cout << "test hash: static: " << h1 << endl;
     cout << "test hash: runtime: " << h2 << endl;
