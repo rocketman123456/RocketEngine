080a9643b87ef6c09f841e8be67b98f17a9f2356
rocketman123456 <759094438@qq.com>
2022-08-20 15:47:56 +0800

    fix bug

--- Library/portaudio/xmake.lua
+++ Library/portaudio/xmake.lua
@@ -14,17 +14,25 @@ target("portaudio")
             -- "src/hostapi/wasapi/*.c"
             -- "src/hostapi/wdmks/*.c"
         )
-        add_packages("winmm", "ole32", "uuid")
         add_defines("PA_USE_WMME")
+        add_packages("winmm", "ole32", "uuid")
     else
         add_files("src/os/unix/*.c")
         if is_plat("macosx") then
             add_files("src/hostapi/coreaudio/*.c")
             add_defines("PA_USE_COREAUDIO")
+            add_frameworks(
+                "CoreAudio", 
+                "AudioToolbox", 
+                "AudioUnit", 
+                "CoreFoundation", 
+                "CoreServices",
+                {public = true}
+            )
         else
             add_files("src/hostapi/alsa/*.c")
-            add_packages("alsa")
             add_defines("PA_USE_ALSA")
+            add_packages("alsa")
         end
     end
 target_end()
